413f95316f5328d56617ca2ff9183d67f08e9372
Thomas Levine <_@thomaslevine.com>
2014-08-27 13:23:41 +0000

    printf

--- compress.hs
+++ compress.hs
@@ -1,9 +1,11 @@
+import Text.Printf (printf)
+
 data Time = Time Int
 
 instance Show Time where
-  show (Time minutes) = (show $ minutes `div` 60) ++ ":" ++ (show $ minutes `mod` 60)
+  show (Time minutes) = (printf "% 2s" $ show $ minutes `div` 60) ++ ":" ++ (printf "%02s" $ show $ minutes `mod` 60)
 
 -- times = [ Time 600 ]
 
 main = do
-  putStrLn $ show $ Time 80
+  putStrLn $ show $ Time 302
