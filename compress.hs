cf3d8a57cc97431b94a9f2493e30e3a1951778f5
Thomas Levine <_@thomaslevine.com>
2014-08-27 13:20:25 +0000

    printing Time type

--- compress.hs
+++ compress.hs
@@ -1,11 +1,9 @@
 data Time = Time Int
 
 instance Show Time where
-  show minutes = (show $ minutes `div` 60) ++ ":" ++ (show minutes `mod` 60)
+  show (Time minutes) = (show $ minutes `div` 60) ++ ":" ++ (show $ minutes `mod` 60)
 
-times = [
-  Time 600
-]
+-- times = [ Time 600 ]
 
 main = do
-  show times
+  putStrLn $ show $ Time 80
