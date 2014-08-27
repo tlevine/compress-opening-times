62bad3874073a15f5611f5cd5474028ff650f820
Thomas Levine <_@thomaslevine.com>
2014-08-27 13:41:48 +0000

    compress times

--- compress.hs
+++ compress.hs
@@ -22,19 +22,17 @@ exactDiv a b
   | a `mod` b == 0 = Just $ a `div` b
   | otherwise = Nothing
 
-compressOpening :: Time -> Maybe Int
-compressOpening (Time minutes) =
-  case compressedOpening minutes of
+compressTime :: (Time -> Maybe Int) Time
+compressTime compressor (Time minutes) =
+  case compressor minutes of
     Just result && result >= 0 -> result
     Just result -> Nothing
     Nothing -> Nothing
   where
-    compressedOpening x = (720 - x) `exactDiv` 30
+    compressor
 
-map (\x -> 360 + (30 * x)) [0..15]
-
-compressOpening :: Time -> Maybe Int
-compressOpening
+compressOpening = compressTime $ (720 - x) `exactDiv` 30
+compressClosing = compressTime $ (1560 - x) `exactDiv` 30
 
 compressDay :: DaySchedule -> String
 compressDay (Time opening, Time closing, days) =
