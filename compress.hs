573c6d9fe389c8777e7b89008cff7477543d9620
Thomas Levine <_@thomaslevine.com>
2014-08-27 13:39:16 +0000

    -

--- compress.hs
+++ compress.hs
@@ -1,3 +1,4 @@
+import qualified Data.Map as M
 import Text.Printf (printf)
 
 data Time = Time Int
@@ -5,18 +6,38 @@ data Time = Time Int
 instance Show Time where
   show (Time minutes) = (printf "% 2s" $ show $ minutes `div` 60) ++ ":" ++ (printf "%02s" $ show $ minutes `mod` 60)
 
---' A weekly schedule
---'
---' A series of tuples starting with Monday, each tuple containing
---' * opening time
---' * closing time
---' * number of days to repeat
---'
---' The sum of the last items of these tuples shall be seven.
-type Schedule = [(Time, Time, Integer)]
+-- A weekly schedule
+--
+-- A series of tuples starting with Monday, each tuple containing
+-- * opening time
+-- * closing time
+-- * number of days to repeat
+--
+-- The sum of the last items of these tuples shall be seven.
+type DaySchedule (Time, Time, Integer)
+type Schedule = [DaySchedule]
 
-compress :: Schedule -> String
-compress (Time minutes) =
+exactDiv :: Int -> Maybe Int
+exactDiv a b
+  | a `mod` b == 0 = Just $ a `div` b
+  | otherwise = Nothing
+
+compressOpening :: Time -> Maybe Int
+compressOpening (Time minutes) =
+  case compressedOpening minutes of
+    Just result && result >= 0 -> result
+    Just result -> Nothing
+    Nothing -> Nothing
+  where
+    compressedOpening x = (720 - x) `exactDiv` 30
+
+map (\x -> 360 + (30 * x)) [0..15]
+
+compressOpening :: Time -> Maybe Int
+compressOpening
+
+compressDay :: DaySchedule -> String
+compressDay (Time opening, Time closing, days) =
 
 main = do
   putStrLn $ show $ Time 302
