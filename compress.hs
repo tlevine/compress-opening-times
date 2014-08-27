5953d74600092746df68da250207d40ee5d18d86
Thomas Levine <_@thomaslevine.com>
2014-08-27 13:59:31 +0000

    -

--- compress.hs
+++ compress.hs
@@ -14,7 +14,7 @@ instance Show Time where
 -- * number of days to repeat
 --
 -- The sum of the last items of these tuples shall be seven.
-type DaySchedule (Time, Time, Integer)
+type DaySchedule = (Time, Time, Integer)
 type Schedule = [DaySchedule]
 
 exactDiv :: Int -> Maybe Int
@@ -25,17 +25,23 @@ exactDiv a b
 compressTime :: (Time -> Maybe Int) Time
 compressTime compressor (Time minutes) =
   case compressor minutes of
-    Just result && result >= 0 -> result
+    Just result && result >= 0 && result <= 15 -> result
     Just result -> Nothing
     Nothing -> Nothing
   where
     compressor
 
 compressOpening = compressTime $ (720 - x) `exactDiv` 30
-compressClosing = compressTime $ (1560 - x) `exactDiv` 30
+compressClosing = compressTime $ (1500 - x) `exactDiv` 30
 
+--' Produce four bits.
+--' If no bits are on, the store is closed.
+--' If one bit is on, use that for both the opening and the closing time.
+--' If two bits are on, use
 compressDay :: DaySchedule -> String
 compressDay (Time opening, Time closing, days) =
+  compressOpening opening
+  compressClosing closing
 
 main = do
   putStrLn $ show $ Time 302
