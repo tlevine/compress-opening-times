c19b39c42331cce7f0ee5a509f8b2a23da518520
Thomas Levine <_@thomaslevine.com>
2014-08-27 13:27:49 +0000

    schedule type

--- compress.hs
+++ compress.hs
@@ -5,7 +5,18 @@ data Time = Time Int
 instance Show Time where
   show (Time minutes) = (printf "% 2s" $ show $ minutes `div` 60) ++ ":" ++ (printf "%02s" $ show $ minutes `mod` 60)
 
--- times = [ Time 600 ]
+--' A weekly schedule
+--'
+--' A series of tuples starting with Monday, each tuple containing
+--' * opening time
+--' * closing time
+--' * number of days to repeat
+--'
+--' The sum of the last items of these tuples shall be seven.
+type Schedule = [(Time, Time, Integer)]
+
+compress :: Schedule -> String
+compress (Time minutes) =
 
 main = do
   putStrLn $ show $ Time 302
